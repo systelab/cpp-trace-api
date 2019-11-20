d829c14e9eac22788d9360440ca2c2f576b2fe1a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-20 23:49:16 +0100

    Added test for Conan package

---
+++ test_package/TraceAPIExample.cpp
@@ -0,0 +1,17 @@
+#define BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <stdexcept>
+
+#include "TraceAPI/TraceAgent.h"
+
+
+int main(int argc, char *argv[])
+{
+	auto agent = std::make_unique<systelab::trace::TraceAgent>();
+	agent->enable(true);
+
+    return 0;
+}
---
+++ test_package/conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, CMake
+
+
+class TraceAPITestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package"
+    options = {"boost": ["1.66.0", "1.67.0"], "gtest": ["1.7.0", "1.8.1"]}
+    default_options = {"boost":"1.67.0", "gtest":"1.8.1"}
+
+    def configure(self):
+        self.options["TraceAPI"].boost = self.options.boost
+        self.options["TraceAPI"].gtest = self.options.gtest
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        cmake = CMake(self)
+        cmake.test()
