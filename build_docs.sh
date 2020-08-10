72c4aec1dea8d6d45069a46d1f3ea32edf48c16c
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-10 12:16:08 +0200

    New attempt to publish github action

--- build_docs.sh
+++ build_docs.sh
@@ -18,16 +18,21 @@ function dispatchDocBuildsEvent
 	CONFIG_NAME="$APPVEYOR_BUILD_WORKER_IMAGE $PLATFORM $CONFIGURATION"
 	JOB_ID=$APPVEYOR_JOB_ID
 
-	echo "\nDispatch GitHub Action to build documentation...\n\n"	
+	echo "Dispatch GitHub Action to build documentation..."	
 	echo "TAG=$TAG_NAME"
 	echo "CI=$CI_NAME"
 	echo "CONFIGURATION=$CONFIG_NAME"
-	echo "JOB=$JOB_ID\n\n"
+	echo "JOB=$JOB_ID"
+	echo ""
+	
+	BODY_CONTENT="{\"event_type\": \"doc-build\", \"client_payload\": {\"tag\": \"$TAG_NAME\", \"configuration\": \"$CONFIG_NAME\", \"ci\": \"$CI_NAME\", \"job\": \"$JOB_ID\"}}"
+	echo "BODY_CONTENT=$BODY_CONTENT"
+	echo ""
 
 	curl -H "Authorization: token $GITHUB_ACTION_DISPATCH_TOKEN" \
 		 -H 'Accept: application/vnd.github.everest-preview+json' \
 		 "https://api.github.com/repos/systelab/cpp-trace-api/dispatches" \
-		 -d '{"event_type": "doc-build", "client_payload": {"tag": "${TAG_NAME}", "configuration": "${CONFIG_NAME}", "ci": "${CI_NAME}", "job": "${JOB_ID}"}}'
+		 -d "$BODY_CONTENT"
 	checkErrors
 	echo "Done.\n"
 }
