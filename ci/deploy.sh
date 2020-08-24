c75142e615cf8857e31ecb67f3d750c6ffd8920d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-24 13:45:07 +0200

    Recovered appveyor deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -39,7 +39,5 @@ function parseParameters
 parseParameters "${@}"
 
 echo -e "\nUploading TraceAPI/$VERSION@systelab/stable to systelab-bintray...\n\n"
-conan user
-conan remote add systelab-bintray https://api.bintray.com/conan/systelab/conan
 conan upload "TraceAPI/$VERSION@systelab/stable" --all -r systelab-bintray --force
 checkErrors
