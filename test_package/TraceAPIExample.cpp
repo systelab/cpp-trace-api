5edf096296245f1c052e1ae7e3203ab7fa8f639d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-09-02 17:46:16 +0200

    Considered config object on conan packaging test

--- test_package/TraceAPIExample.cpp
+++ test_package/TraceAPIExample.cpp
@@ -6,12 +6,17 @@
 #include <stdexcept>
 
 #include "TraceAPI/ChannelMacro.h"
+#include "TraceAPI/Configuration.h"
 #include "TraceAPI/FileAgent.h"
 
 
 int main(int argc, char *argv[])
 {
-	auto fileAgent = std::make_unique<systelab::trace::FileAgent>("Channel", "Filename", "FolderPath", 3);
+	auto configuration = std::make_unique<systelab::trace::Configuration>();
+	configuration->setChannelName("MyChannel");
+	configuration->setBaseFolderPath("FolderPath");
+
+	auto fileAgent = std::make_unique<systelab::trace::FileAgent>(std::move(configuration));
 	fileAgent->enable(true);
 	TRACE_CHANNEL("Channel") << "The Conan package has been created successfully!";
 	fileAgent->flush();
