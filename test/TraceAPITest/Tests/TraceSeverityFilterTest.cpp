e6c6a44b15d69bd30c6563fc8b871db82aa52056
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-09-07 11:19:04 +0200

    Completed tests for severity filter

--- test/TraceAPITest/Tests/TraceSeverityFilterTest.cpp
+++ test/TraceAPITest/Tests/TraceSeverityFilterTest.cpp
@@ -67,5 +67,38 @@ namespace systelab { namespace trace { namespace unit_test {
 		EXPECT_TRUE(assertTraceLineSeverity(traceFileLines[3], "ERROR", "This is ERROR trace 2."));
 	}
 
+	TEST_F(TraceSeverityFilterTest, testOnlyErrorTracesWithTagAddedToFile)
+	{
+		configureSeverityFilter({ "DEBUG" });
+
+		for (unsigned int i = 1; i <= 3; i++)
+		{
+			TRACE_CHANNEL_SEVERITY_TAG(m_channelName, "DEBUG", "LOOP" + std::to_string(i)) << "This is DEBUG trace " << i << ".";
+			TRACE_CHANNEL_SEVERITY_TAG(m_channelName, "INFO",  "LOOP" + std::to_string(i)) << "This is INFO trace " << i << ".";
+			TRACE_CHANNEL_SEVERITY_TAG(m_channelName, "ERROR", "LOOP" + std::to_string(i)) << "This is ERROR trace " << i << ".";
+		}
+		m_fileAgent->flush();
+
+		auto traceFileLines = readTraceFile();
+		ASSERT_EQ(3, traceFileLines.size());
+		EXPECT_TRUE(assertTraceLineSeverityTag(traceFileLines[0], "DEBUG", "LOOP1", "This is DEBUG trace 1."));
+		EXPECT_TRUE(assertTraceLineSeverityTag(traceFileLines[1], "DEBUG", "LOOP2", "This is DEBUG trace 2."));
+		EXPECT_TRUE(assertTraceLineSeverityTag(traceFileLines[2], "DEBUG", "LOOP3", "This is DEBUG trace 3."));
+	}
+
+	TEST_F(TraceSeverityFilterTest, testTracesWithoutSeverityNotAddedToFile)
+	{
+		configureSeverityFilter({ "DEBUG" });
+
+		TRACE_CHANNEL(m_channelName) << "This is no severity nor tag trace " << 1 << ".";
+		TRACE_CHANNEL(m_channelName) << "This is no severity nor tag trace " << 2 << ".";
+		TRACE_CHANNEL_TAG(m_channelName, "TAG1") << "This is no severity with tag '" << "TAG1" << "' trace.";
+		TRACE_CHANNEL_TAG(m_channelName, "TAG2") << "This is no severity with tag '" << "TAG2" << "' trace.";
+		m_fileAgent->flush();
+
+		auto traceFileLines = readTraceFile();
+		ASSERT_EQ(0, traceFileLines.size());
+	}
+
 }}}
 
