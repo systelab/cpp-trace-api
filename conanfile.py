0659ca6c410f0576fe75d1331a4ff694fd739c8e
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-10-01 15:35:52 +0200

    Added hash for gtest 1.10.0

--- conanfile.py
+++ conanfile.py
@@ -31,8 +31,10 @@ class TraceAPIConan(ConanFile):
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
             self.build_requires("gtest/1.8.1")
+        elif self.options.gtest == "1.10.0":
+            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
         else:
-            self.build_requires("gtest/1.10.0")
+            self.build_requires(f"gtest/{self.options.gtest}")
 
     def build(self):
         cmake = CMake(self)
