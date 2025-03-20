1b7403f5edaabb36a2c014a4fdf8ad2bfae1d7fe
jlpeiro <jpeiro@werfen.com>
2025-03-20 15:14:35 +0100

    Modified conanfiles

--- conanfile.py
+++ conanfile.py
@@ -10,37 +10,13 @@ class TraceAPIConan(ConanFile):
     license = "MIT"
     generators = "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"boost": ["1.66.0", "1.67.0", "1.72.0", "1.75.0", "1.76.0"], "gtest": ["1.7.0", "1.8.1", "1.10.0"]}
-    default_options = {"boost":"1.76.0", "gtest":"1.10.0"}
-    exports_sources = "*"
     required_conan_version = ">=1.33.1"
 
-    def configure(self):
-        self.options["boost"].shared = True
-
     def requirements(self):
-        if self.options.boost == "1.66.0":
-            self.requires("boost/1.66.0@conan/stable")
-        elif self.options.boost == "1.67.0":
-            self.requires("boost/1.67.0@conan/stable")
-        elif self.options.boost == "1.72.0":
-            self.requires("boost/1.72.0#50e13743ed3c867fe95f151e113bc1ba")
-        elif self.options.boost == "1.75.0":
-            self.requires("boost/1.75.0#32c81e4e89c54b95b2c2c198fe3cb65f")
-        elif self.options.boost == "1.76.0":
-            self.requires("boost/1.76.0#329123a826d0a53fef8318ec3e729a52")
-        else:
-            self.requires(("boost/%s") % self.options.boost)
+        self.requires("boost/1.85.0#0734cd0dd8fe650aa3ae64bb51bca54d")
 
     def build_requirements(self):
-        if self.options.gtest == "1.7.0":
-            self.build_requires("gtest/1.7.0@systelab/stable")
-        elif self.options.gtest == "1.8.1":
-            self.build_requires("gtest/1.8.1")
-        elif self.options.gtest == "1.10.0":
-            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
-        else:
-            self.build_requires(f"gtest/{self.options.gtest}")
+        self.build_requires("gtest/1.14.0#4372c5aed2b4018ed9f9da3e218d18b3")
 
     def build(self):
         cmake = CMake(self)
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,13 +4,7 @@ from conans import ConanFile, CMake
 class TraceAPITestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"boost": ["1.66.0", "1.67.0", "1.72.0", "1.75.0"], "gtest": ["1.7.0", "1.8.1", "1.10.0"]}
-    default_options = {"boost":"1.72.0", "gtest":"1.10.0"}
     required_conan_version = ">=1.33.1"
-
-    def configure(self):
-        self.options["TraceAPI"].boost = self.options.boost
-        self.options["TraceAPI"].gtest = self.options.gtest
 
     def build(self):
         cmake = CMake(self)
